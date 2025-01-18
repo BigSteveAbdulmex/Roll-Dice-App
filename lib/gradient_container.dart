5b409c975acc1c6995fd53b5e0fb47ca355e60b2
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-01-18 01:00:58 +0100

    Implemented features for the dice rolling app

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:roll_dice_app/dice_roller.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
